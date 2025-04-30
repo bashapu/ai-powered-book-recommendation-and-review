1fda09482289857f848ac4b62470bcce7e661930
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-04-30 12:30:27 -0400

    Add Basic My Library Screen

---
+++ lib/screens/my_library_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyLibraryScreen extends StatelessWidget {
+  const MyLibraryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('My Library - Coming Soon'),
+    );
+  }
+}
