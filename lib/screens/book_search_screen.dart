87644c1fa40dd90a568b0718245cf352653bbdfc
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-04-30 12:29:43 -0400

    Add Basic Book Search Screen

---
+++ lib/screens/book_search_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookSearchScreen extends StatelessWidget {
+  const BookSearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Search Screen - Coming Soon'),
+    );
+  }
+}
