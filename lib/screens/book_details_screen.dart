879908a18047365c6f485bbb0312b669c07488f2
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-05-01 02:27:45 -0400

    Update Book Details Screen with Button to Add the Book to Library

--- lib/screens/book_details_screen.dart
+++ lib/screens/book_details_screen.dart
@@ -1,3 +1,5 @@
+import 'package:book_app/models/book_model.dart';
+import 'package:book_app/services/library_service.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsScreen extends StatelessWidget {
@@ -24,7 +26,16 @@ class BookDetailsScreen extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // TODO: Add to My Library
+                final bookModel = BookModel(
+                  id: book['id'],
+                  title: book['title'],
+                  authors: book['authors'],
+                  thumbnail: book['thumbnail'],
+                  description: book['description'],
+                  status: 'want_to_read', // default
+                );
+                LibraryService().addBook(bookModel);
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Book added to your library!')));
               },
               child: const Text('Add to My Library'),
             ),
