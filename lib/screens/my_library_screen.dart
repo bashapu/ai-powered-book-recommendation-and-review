29199e422e2db5445415486c34828e7a3c161dca
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-05-01 02:26:06 -0400

    Update My Library Screen with Status Tabs and Firebase Sync on Books

--- lib/screens/my_library_screen.dart
+++ lib/screens/my_library_screen.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
+import '../models/book_model.dart';
+import '../services/library_service.dart';
 
-class MyLibraryScreen extends StatelessWidget {
+class MyLibraryScreen extends StatefulWidget {
   const MyLibraryScreen({super.key});
 
   @override
+  State<MyLibraryScreen> createState() => _MyLibraryScreenState();
+}
+
+class _MyLibraryScreenState extends State<MyLibraryScreen>
+    with SingleTickerProviderStateMixin {
+  final LibraryService _libraryService = LibraryService();
+  late TabController _tabController;
+
+  final List<String> _statuses = [
+    'want_to_read',
+    'currently_reading',
+    'finished'
+  ];
+
+  @override
+  void initState() {
+    _tabController = TabController(length: _statuses.length, vsync: this);
+    super.initState();
+  }
+
+  Widget _buildBookCard(BookModel book) {
+    return ListTile(
+      leading: book.thumbnail != null
+          ? Image.network(book.thumbnail!, width: 50)
+          : const Icon(Icons.book),
+      title: Text(book.title),
+      subtitle: Text(book.authors),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('My Library - Coming Soon'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Library'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Want to Read'),
+            Tab(text: 'Currently Reading'),
+            Tab(text: 'Finished'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: _statuses.map((status) {
+          return StreamBuilder<List<BookModel>>(
+            stream: _libraryService.getBooksByStatus(status),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Center(child: CircularProgressIndicator());
+              final books = snapshot.data!;
+              return ListView.builder(
+                itemCount: books.length,
+                itemBuilder: (_, i) => _buildBookCard(books[i]),
+              );
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
